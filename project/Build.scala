a3dd901cc76f15ec790d3f22e0f9de81e24e6406
Rikito Taniguchi <rikiriki1238@gmail.com>
2026-01-13 17:07:33 +0900

    Enable tests for toString with radix

--- project/Build.scala
+++ project/Build.scala
@@ -2358,9 +2358,6 @@ object Build {
                 // scalalib
                 !endsWith(f, "/scalalib/SymbolTest.scala") && // Symbol#JSUniquenessCache
 
-                // javalib/math
-                !endsWith(f, "/javalib/math/BigIntegerToStringTest.scala") && // Long.toStringImpl
-
                 // javalib/lang
                 !endsWith(f, "/lang/ClassValueTest.scala") && // js.Map in ClassValue
                 !endsWith(f, "/lang/SystemPropertiesTest.scala") && // dictionary in SystemProperties
@@ -2368,7 +2365,6 @@ object Build {
                 // javalib/util
                 !endsWith(f, "/FormatterTest.scala") &&
                 !endsWith(f, "/ArraysTest.scala") && // Arrays.deepEquals
-                !endsWith(f, "/IntConsumerTest.scala") && // Long#StringRadixInfos
                 !endsWith(f, "/DateTest.scala") && // js.Date
                 !endsWith(f, "/PropertiesTest.scala") && // Date.toString
 
